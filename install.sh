b572f5cbe1e617cc338f5677f82ce3c659d77fac
Daniel Hjertholm <daniel.hjertholm@icloud.com>
2021-10-02 15:14:47 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Ensure the Fira Code Nerd Font is installed.
+
+# Ensure starship is installed.
+
+# Ensure starship config is downloaded.
+
+# Ensure starship is initialized.
+
+# Ensure terminal dotfile is updated to initialize starship on every new terminal session.
