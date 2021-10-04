1f47ac46333e8cf217bea78f03991a3c544cb0f3
Daniel Hjertholm <daniel.hjertholm@icloud.com>
2021-10-04 10:21:11 +0200

    Don't download fonts if already present

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # MacOS
     mkdir -p ~/Library/Fonts/
-    for type in Bold Light Medium Regular Retina; do wget -O ~/Library/Fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; done
+    for type in Bold Light Medium Regular Retina; do wget -nc -O ~/Library/Fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; done
 else
     mkdir -p ~/.local/share/fonts/
-    for type in Bold Light Medium Regular Retina; do wget -O ~/.local/share/fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; done
+    for type in Bold Light Medium Regular Retina; do wget -nc -O ~/.local/share/fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; done
     fc-cache -f
 fi
 cd -
